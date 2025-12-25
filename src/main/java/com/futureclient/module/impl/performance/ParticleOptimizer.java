dabd52c1b2087592f053625ffc30c2434990744f
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-12-25 15:40:51 +0530

    Create ParticleOptimizer.java

---
+++ src/main/java/com/futureclient/module/impl/performance/ParticleOptimizer.java
@@ -0,0 +1,11 @@
+package com.futureclient.module.impl.performance;
+
+import com.futureclient.module.Category;
+import com.futureclient.module.Module;
+
+public class ParticleOptimizer extends Module {
+    public ParticleOptimizer() {
+        super("ParticleOptimizer", "Reduces particles for FPS", Category.PERFORMANCE);
+    }
+    // Logic checked in Mixins or Render events
+}
