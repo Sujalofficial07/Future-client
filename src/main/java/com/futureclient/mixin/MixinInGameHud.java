ec03cd2be1cd1bff0bd7eb5d3d2cb5b4173cc0d6
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-12-25 16:36:22 +0530

    Update MixinInGameHud.java

--- src/main/java/com/futureclient/mixin/MixinInGameHud.java
+++ src/main/java/com/futureclient/mixin/MixinInGameHud.java
@@ -2,16 +2,18 @@ package com.futureclient.mixin;
 
 import com.futureclient.FutureClient;
 import com.futureclient.event.impl.EventRender2D;
-import net.minecraft.client.gui.Hud;
+import net.minecraft.client.gui.hud.InGameHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Hud.class)
+@Mixin(InGameHud.class)
 public class MixinInGameHud {
     @Inject(method = "render", at = @At("RETURN"))
     public void onRender(float tickDelta, CallbackInfo ci) {
-        FutureClient.INSTANCE.eventBus.post(new EventRender2D(tickDelta));
+        if (FutureClient.INSTANCE != null && FutureClient.INSTANCE.eventBus != null) {
+            FutureClient.INSTANCE.eventBus.post(new EventRender2D(tickDelta));
+        }
     }
 }
