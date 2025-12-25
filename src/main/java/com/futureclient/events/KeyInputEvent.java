a17f310da8574ed6f02bec1fec219e1072f667a9
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-12-25 15:23:48 +0530

    Delete src/main directory

--- src/main/java/com/futureclient/events/KeyInputEvent.java
+++
@@ -1,8 +0,0 @@
-package com.futureclient.events;
-
-/**
- * Event fired when a key is pressed
- */
-public class KeyInputEvent {
-    // Empty event class for key input notifications
-}
--- src/main/java/com/futureclient/events/TickEvent.java
+++
@@ -1,8 +0,0 @@
-package com.futureclient.events;
-
-/**
- * Event fired every client tick
- */
-public class TickEvent {
-    // Empty event class for tick notifications
-}
