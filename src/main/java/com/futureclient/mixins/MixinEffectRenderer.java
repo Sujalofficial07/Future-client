f48e24acaccad944610abb0ed9861197726b5c6d
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-12-25 14:41:08 +0530

    Update and rename MixinParticleManager.java to MixinEffectRenderer.java

--- src/main/java/com/futureclient/mixins/MixinEffectRenderer.java
+++ src/main/java/com/futureclient/mixins/MixinEffectRenderer.java
@@ -2,23 +2,20 @@ package com.futureclient.mixins;
 
 import com.futureclient.FutureClient;
 import com.futureclient.modules.performance.ParticleReducerModule;
-import net.minecraft.client.particle.Particle;
-import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.client.particle.EffectRenderer;
+import net.minecraft.client.particle.EntityFX;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-/**
- * Mixin for reducing particle count
- */
-@Mixin(ParticleManager.class)
-public class MixinParticleManager {
+@Mixin(EffectRenderer.class)
+public class MixinEffectRenderer {
     
     private int particleCounter = 0;
     
-    @Inject(method = "addParticle(Lnet/minecraft/client/particle/Particle;)V", at = @At("HEAD"), cancellable = true)
-    private void onAddParticle(Particle particle, CallbackInfo ci) {
+    @Inject(method = "addEffect", at = @At("HEAD"), cancellable = true)
+    private void onAddEffect(EntityFX particle, CallbackInfo ci) {
         if (FutureClient.getInstance() != null) {
             ParticleReducerModule reducer = (ParticleReducerModule) FutureClient.getInstance()
                 .getModuleManager()
