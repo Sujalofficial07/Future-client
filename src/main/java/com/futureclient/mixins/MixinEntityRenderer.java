27ea8cad1f69a92405f588536975336e26ee23c4
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-12-25 14:40:03 +0530

    Update and rename MixinGameRenderer.java to MixinEntityRenderer.java

--- src/main/java/com/futureclient/mixins/MixinEntityRenderer.java
+++ src/main/java/com/futureclient/mixins/MixinEntityRenderer.java
@@ -2,20 +2,17 @@ package com.futureclient.mixins;
 
 import com.futureclient.FutureClient;
 import com.futureclient.modules.render.ZoomModule;
-import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.renderer.EntityRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-/**
- * Mixin for game renderer to handle FOV modifications
- */
-@Mixin(GameRenderer.class)
-public class MixinGameRenderer {
+@Mixin(EntityRenderer.class)
+public class MixinEntityRenderer {
     
-    @Inject(method = "getFov", at = @At("RETURN"), cancellable = true)
-    private void onGetFov(float partialTicks, boolean useFovSetting, CallbackInfoReturnable<Float> cir) {
+    @Inject(method = "getFOVModifier", at = @At("RETURN"), cancellable = true)
+    private void onGetFOVModifier(float partialTicks, boolean useFovSetting, CallbackInfoReturnable<Float> cir) {
         if (FutureClient.getInstance() != null) {
             ZoomModule zoom = (ZoomModule) FutureClient.getInstance()
                 .getModuleManager()
