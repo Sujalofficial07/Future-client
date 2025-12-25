0e37f7fc393ef2cf2b71a0778868db1798e976ec
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-12-25 16:37:05 +0530

    Update InfoHUD.java

--- src/main/java/com/futureclient/module/impl/hud/InfoHUD.java
+++ src/main/java/com/futureclient/module/impl/hud/InfoHUD.java
@@ -4,7 +4,6 @@ import com.futureclient.event.Event;
 import com.futureclient.event.impl.EventRender2D;
 import com.futureclient.module.Category;
 import com.futureclient.module.Module;
-import net.minecraft.client.gui.Gui;
 
 public class InfoHUD extends Module {
     public InfoHUD() {
@@ -17,8 +16,13 @@ public class InfoHUD extends Module {
         if (event instanceof EventRender2D) {
             if (mc.options.debugEnabled) return;
             
-            String fps = "FPS: " + net.minecraft.client.MinecraftClient.getCurrentFps();
-            String xyz = String.format("XYZ: %.1f %.1f %.1f", mc.player.x, mc.player.y, mc.player.z);
+            // Accessing FPS via the instance 'mc' inherited from Module class
+            String fps = "FPS: " + mc.getCurrentFps();
+            
+            String xyz = "XYZ: 0 0 0";
+            if (mc.player != null) {
+                xyz = String.format("XYZ: %.1f %.1f %.1f", mc.player.x, mc.player.y, mc.player.z);
+            }
             
             mc.textRenderer.drawWithShadow(fps, 2, 2, -1);
             mc.textRenderer.drawWithShadow(xyz, 2, 12, -1);
