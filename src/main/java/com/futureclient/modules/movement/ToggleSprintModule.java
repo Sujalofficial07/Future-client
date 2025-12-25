8fb2cb83605fd0cdf21d581bf30cf7e6ce270d6a
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-12-25 14:30:11 +0530

    Update ToggleSprintModule.java

--- src/main/java/com/futureclient/modules/movement/ToggleSprintModule.java
+++ src/main/java/com/futureclient/modules/movement/ToggleSprintModule.java
@@ -3,23 +3,20 @@ package com.futureclient.modules.movement;
 import com.futureclient.api.Category;
 import com.futureclient.api.Module;
 
-/**
- * Automatically sprint when moving forward
- */
 public class ToggleSprintModule extends Module {
-    
     public ToggleSprintModule() {
         super("ToggleSprint", "Automatically sprint", Category.MOVEMENT);
     }
     
     @Override
     public void onTick() {
-        if (mc.player == null) return;
+        if (mc.thePlayer == null) return;
         
-        // Auto sprint when moving forward
-        if (mc.options.keyForward.isPressed() && !mc.player.horizontalCollision && !mc.player.isSneaking()) {
-            if (!mc.player.isSprinting() && mc.player.getHungerManager().getFoodLevel() > 6) {
-                mc.player.setSprinting(true);
+        if (mc.gameSettings.keyBindForward.isKeyDown() && 
+            !mc.thePlayer.isCollidedHorizontally && 
+            !mc.thePlayer.isSneaking()) {
+            if (!mc.thePlayer.isSprinting() && mc.thePlayer.getFoodStats().getFoodLevel() > 6) {
+                mc.thePlayer.setSprinting(true);
             }
         }
     }
