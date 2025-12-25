e6470107f06ba1cc96bb6abb8c24fa4e71649626
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-12-25 14:31:29 +0530

    Update ParticleReducerModule.java

--- src/main/java/com/futureclient/modules/performance/ParticleReducerModule.java
+++ src/main/java/com/futureclient/modules/performance/ParticleReducerModule.java
@@ -3,12 +3,7 @@ package com.futureclient.modules.performance;
 import com.futureclient.api.Category;
 import com.futureclient.api.Module;
 
-/**
- * Reduces particle count for better performance
- * Note: Actual particle reduction is handled by MixinParticleManager
- */
 public class ParticleReducerModule extends Module {
-    
     public ParticleReducerModule() {
         super("ParticleReducer", "Reduce particle count for FPS", Category.PERFORMANCE);
     }
