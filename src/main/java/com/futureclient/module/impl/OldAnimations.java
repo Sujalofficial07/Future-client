b10b350a4744cda158498a45b3e3d7cafc5f16a3
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-12-30 17:52:05 +0530

    Create OldAnimations.java

---
+++ src/main/java/com/futureclient/module/impl/OldAnimations.java
@@ -0,0 +1,11 @@
+package com.futureclient.module.impl;
+
+import com.futureclient.module.Category;
+import com.futureclient.module.Module;
+
+public class OldAnimations extends Module {
+    public OldAnimations() {
+        super("OldAnimations", "1.8 Style hit/block visuals.", Category.RENDER);
+    }
+    // Logic inside HeldItemRendererMixin
+}
