b873e2208df5f9a787ea2362f36304d4f8b0bc97
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-12-30 17:55:06 +0530

    Create ModuleConfig.java

---
+++ src/main/java/com/futureclient/config/ModuleConfig.java
@@ -0,0 +1,8 @@
+package com.futureclient.config;
+
+public class ModuleConfig {
+    public String name;
+    public boolean enabled;
+    public int key;
+    public ModuleConfig(String n, boolean e, int k) { name=n; enabled=e; key=k; }
+}
