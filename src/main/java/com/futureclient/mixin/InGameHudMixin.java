610776934c18dbad2564e6e262330748be1a9b2f
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-12-30 17:53:38 +0530

    Create InGameHudMixin.java

---
+++ src/main/java/com/futureclient/mixin/InGameHudMixin.java
@@ -0,0 +1,17 @@
+package com.futureclient.mixin;
+
+import com.futureclient.FutureClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+    @Inject(method = "render", at = @At("TAIL"))
+    public void onRender(DrawContext context, float tickDelta, CallbackInfo ci) {
+        FutureClient.hudManager.render(context, tickDelta);
+    }
+}
